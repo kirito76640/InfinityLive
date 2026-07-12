aed99b99d12818c7f746db927a6add8006da0837
kirito76640 <slhommet93@gmail.com>
2026-07-12 21:21:50 +0200

    Premier commit InfinityLive

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+app.use(express.static("public"));
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const PORT: number = Number(process.env.PORT) || 3000;
+
+app.get("/", (req, res) => {
+  res.send("InfinityLive serveur en ligne !");
+});
+
+io.on("connection", (socket) => {
+  console.log("Nouvelle connexion :", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Déconnexion :", socket.id);
+  });
+});
+
+httpServer.listen(PORT, "0.0.0.0", () => {
+  console.log(`InfinityLive serveur démarré sur le port ${PORT}`);
+});
